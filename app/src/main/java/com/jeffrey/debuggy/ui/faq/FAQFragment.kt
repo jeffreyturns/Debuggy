7cc2f8a88ed70b9acaecb3924386428cb43055a5
jeffrey01596 <51068771+Jeffrey01596@users.noreply.github.com>
2021-10-08 00:27:19 +0300

    • [Fix][FAQFragment] fixed fragment crash

--- app/src/main/java/com/jeffrey/debuggy/ui/faq/FAQFragment.kt
+++ app/src/main/java/com/jeffrey/debuggy/ui/faq/FAQFragment.kt
@@ -1,9 +1,5 @@
 package com.jeffrey.debuggy.ui.faq
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.jeffrey.debuggy.data.faq.FAQAdapter
 import com.jeffrey.debuggy.data.faq.faqList
@@ -14,19 +10,12 @@ import com.jeffrey.debuggy.utils.extensions.addInsetPaddings
 
 class FAQFragment : BaseFragment<FragmentFaqBinding>(FragmentFaqBinding::inflate) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
+    override fun setUpViews() {
         enterTransition = TransitionUtil.getMaterialSharedAxis(requireActivity(), false)
         returnTransition = TransitionUtil.getMaterialSharedAxis(requireActivity(), true)
-        val view = binding.root
 
         binding.recyclerView.addInsetPaddings(bottom = true)
         binding.recyclerView.layoutManager = LinearLayoutManager(requireActivity())
         binding.recyclerView.adapter = FAQAdapter(faqList(requireActivity()))
-
-        return view
     }
 }
