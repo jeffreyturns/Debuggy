09f26a425fe7350b04e017e8a6061c59de59f54f
jeffrey01596 <51068771+Jeffrey01596@users.noreply.github.com>
2021-10-09 18:01:55 +0300

    • [Fix][AppModule] fixed app crash in AppModule.kt

--- app/src/main/java/com/jeffrey/debuggy/AppModule.kt
+++ app/src/main/java/com/jeffrey/debuggy/AppModule.kt
@@ -4,18 +4,17 @@ import android.app.Application
 import com.jeffrey.debuggy.di.helperModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
-import org.koin.dsl.module
 
 open class AppModule : Application() {
 
     fun Application.initModule() {
         startKoin {
             androidContext(this@initModule)
-            modules(listOf(modules))
+            modules(modules)
         }
     }
 
-    private val modules = module {
+    private val modules = listOf(
         helperModule
-    }
+    )
 }
