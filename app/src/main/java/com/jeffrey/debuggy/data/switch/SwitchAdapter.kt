196b5215220ce6bcf89e0752876dd8944224b496
jeffrey01596 <51068771+Jeffrey01596@users.noreply.github.com>
2021-12-28 16:57:54 +0200

    • [Update][SwitchAdapter] Smoothly change background color according to state

--- app/src/main/java/com/jeffrey/debuggy/data/switch/SwitchAdapter.kt
+++ app/src/main/java/com/jeffrey/debuggy/data/switch/SwitchAdapter.kt
@@ -1,15 +1,21 @@
 package com.jeffrey.debuggy.data.switch
 
+import android.animation.ArgbEvaluator
+import android.animation.ValueAnimator
 import android.content.Context
+import android.graphics.Color
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.jeffrey.debuggy.R
 import com.jeffrey.debuggy.data.preference.PreferencesHelper
 import com.jeffrey.debuggy.databinding.ItemSwitchBinding
 import com.jeffrey.debuggy.ui.base.BaseViewHolder
 import com.jeffrey.debuggy.ui.main.MainActivity
+import com.jeffrey.debuggy.util.extensions.getAttr
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
+
 
 class SwitchAdapter(private val context: Context) :
     RecyclerView.Adapter<BaseViewHolder>(), KoinComponent {
@@ -27,14 +33,36 @@ class SwitchAdapter(private val context: Context) :
 
     override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
         with(holder.binding as ItemSwitchBinding) {
+            masterSurface.setBackgroundColor(if (preference.adbEnabled) context.getAttr(R.attr.colorSecondary) else Color.WHITE)
             masterSwitch.text = title
             masterSwitch.isChecked = preference.adbEnabled
             masterSwitch.setOnClickListener {
                 (context as MainActivity).tcpStatus()
                 masterSwitch.text = title
+                changeColor(masterSurface)
             }
-
         }
+    }
+
+    private fun changeColor(surface: ConstraintLayout) {
+        val fromColor =
+            if (preference.adbEnabled) context.getAttr(R.attr.colorSecondary) else Color.WHITE
+        val toColor =
+            if (!preference.adbEnabled) context.getAttr(R.attr.colorSecondary) else Color.WHITE
+
+        val colorAnimation: ValueAnimator =
+            ValueAnimator.ofObject(
+                ArgbEvaluator(),
+                toColor,
+                fromColor
+            )
+        colorAnimation.duration = 500
+        colorAnimation.addUpdateListener { animator ->
+            surface.setBackgroundColor(
+                animator.animatedValue as Int
+            )
+        }
+        colorAnimation.start()
     }
 
     override fun getItemCount() = 1
