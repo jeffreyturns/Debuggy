3e5b5ff352e8dafca1d4f51c5867674293687429
jeffrey01596 <51068771+Jeffrey01596@users.noreply.github.com>
2022-01-02 17:54:23 +0200

    • [Update][SettingsFragment] Sections now use forEach function to make code more readable • [Update][ContextExtensions] Add helper methods for navController functions

--- app/src/main/java/com/jeffrey/debuggy/ui/setting/SettingsFragment.kt
+++ app/src/main/java/com/jeffrey/debuggy/ui/setting/SettingsFragment.kt
@@ -1,71 +1,36 @@
 package com.jeffrey.debuggy.ui.setting
 
 import android.os.Bundle
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
 import androidx.preference.Preference
 import com.jeffrey.debuggy.R
 import com.jeffrey.debuggy.ui.base.BasePreferenceFragmentCompat
 import com.jeffrey.debuggy.util.TransitionUtils
+import com.jeffrey.debuggy.util.extensions.navigate
 
 class SettingsFragment : BasePreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences_settings, rootKey)
 
-        val general: Preference = findPreference("general")!!
-        general.setOnPreferenceClickListener {
-            val navController: NavController =
-                Navigation.findNavController(requireActivity(), R.id.nav_host_fragment)
-            navController.navigate(R.id.navigation_general_settings)
-            exitTransition = TransitionUtils.getMaterialSharedAxis(requireActivity(), false)
-            reenterTransition = TransitionUtils.getMaterialSharedAxis(requireActivity(), true)
+        val preferences = listOf("general", "appearance", "security", "experimental", "about")
+        val destinations = listOf(
+            R.id.navigation_general_settings,
+            R.id.navigation_appearance_settings,
+            R.id.navigation_security_settings,
+            R.id.navigation_experimental_settings,
+            R.id.navigation_about_settings
+        )
 
-            true
-        }
+        preferences.zip(destinations).forEach { pair ->
+            val preference: Preference = findPreference(pair.component1())!!
 
-        val appearance: Preference = findPreference("appearance")!!
-        appearance.setOnPreferenceClickListener {
-            val navController: NavController =
-                Navigation.findNavController(requireActivity(), R.id.nav_host_fragment)
-            navController.navigate(R.id.navigation_appearance_settings)
-            exitTransition = TransitionUtils.getMaterialSharedAxis(requireActivity(), false)
-            reenterTransition = TransitionUtils.getMaterialSharedAxis(requireActivity(), true)
+            preference.setOnPreferenceClickListener {
+                requireActivity().navigate(pair.component2())
+                exitTransition = TransitionUtils.getMaterialSharedAxis(requireActivity(), false)
+                reenterTransition = TransitionUtils.getMaterialSharedAxis(requireActivity(), true)
 
-            true
-        }
-
-        val security: Preference = findPreference("security")!!
-        security.setOnPreferenceClickListener {
-            val navController: NavController =
-                Navigation.findNavController(requireActivity(), R.id.nav_host_fragment)
-            navController.navigate(R.id.navigation_security_settings)
-            exitTransition = TransitionUtils.getMaterialSharedAxis(requireActivity(), false)
-            reenterTransition = TransitionUtils.getMaterialSharedAxis(requireActivity(), true)
-
-            true
-        }
-
-        val experimental: Preference = findPreference("experimental")!!
-        experimental.setOnPreferenceClickListener {
-            val navController: NavController =
-                Navigation.findNavController(requireActivity(), R.id.nav_host_fragment)
-            navController.navigate(R.id.navigation_experimental_settings)
-            exitTransition = TransitionUtils.getMaterialSharedAxis(requireActivity(), false)
-            reenterTransition = TransitionUtils.getMaterialSharedAxis(requireActivity(), true)
-
-            true
-        }
-
-        val about: Preference = findPreference("about")!!
-        about.setOnPreferenceClickListener {
-            val navController: NavController =
-                Navigation.findNavController(requireActivity(), R.id.nav_host_fragment)
-            navController.navigate(R.id.navigation_about_settings)
-            exitTransition = TransitionUtils.getMaterialSharedAxis(requireActivity(), false)
-            reenterTransition = TransitionUtils.getMaterialSharedAxis(requireActivity(), true)
-
-            true
+                true
+            }
         }
     }
 }
--- app/src/main/java/com/jeffrey/debuggy/util/extensions/ContextExtensions.kt
+++ app/src/main/java/com/jeffrey/debuggy/util/extensions/ContextExtensions.kt
@@ -3,7 +3,10 @@ package com.jeffrey.debuggy.util.extensions
 import android.content.Context
 import android.util.TypedValue
 import android.widget.Toast
-
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.jeffrey.debuggy.R
 
 fun Context.toast(text: String?, duration: Int = Toast.LENGTH_SHORT) {
     Toast.makeText(this, text.orEmpty(), duration).show()
@@ -21,4 +24,20 @@ fun Context.getAttr(resId: Int): Int {
     val typedValue = TypedValue()
     this.theme.resolveAttribute(resId, typedValue, true)
     return typedValue.data
+}
+
+fun Context.navigate(destination: Int) = Navigation.findNavController(
+    (this as AppCompatActivity),
+    R.id.nav_host_fragment
+).navigate(destination)
+
+fun Context.navigateUp() = Navigation.findNavController(
+    (this as AppCompatActivity),
+    R.id.nav_host_fragment
+).navigateUp()
+
+
+fun Context.getCurrentFragment(): Fragment {
+    return (this as AppCompatActivity).supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+        ?.childFragmentManager?.fragments?.get(0)!!
 }
